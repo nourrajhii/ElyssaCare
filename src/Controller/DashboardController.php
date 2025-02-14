5e05068df1c86c1c41243b02f01d6d3a1fe7ae08
nour <nour.rajhi@esprit.tn>
2025-02-14 21:13:27 +0100

    Ajout de nouveaux fichiers pour le dashboard, flags et autres ressources

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/back', name: 'app_dashboard')]
+    public function index(SessionInterface $session, UserRepository $userRepository): Response
+    {
+        $roleCounts = $userRepository->countUsersByRole();
+
+        // Calculer le pourcentage
+        $totalUsers = array_sum($roleCounts);
+        $percentages = [];
+    
+        if ($totalUsers > 0) {
+            foreach ($roleCounts as $role => $count) {
+                $percentages[$role] = round(($count / $totalUsers) * 100, 2);
+            }
+        }
+    
+        return $this->render('dashboard/back.html.twig', [
+            'percentages' => $percentages,
+        ]);
+    }
+    
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('dashboard/about.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+    #[Route('/evnts', name: 'app_events')]
+    public function events(): Response
+    {
+        return $this->render('dashboard/events.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+    
+}
